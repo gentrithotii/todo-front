3ee8f8c97c0e157f4fc7716b568e07a67890a206
Gentrithotii <Gentrithoti@hotmail.com>
2025-09-04 16:43:25 +0200

    Removed debug code

--- src/services/userService.js
+++ src/services/userService.js
@@ -46,12 +46,6 @@ export const updateUser = async (id, data) => {
       authConfig()
     );
 
-    if (response.status === 204) {
-      console.log("User updated");
-    } else {
-      console.log("Response:", response);
-    }
-
     return response.status;
   } catch (err) {
     console.error("Could not update user with id:", id, err);
